cf40bc07ad7339c24d136458cff809668ceb6944
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2025-10-06 17:50:45 +0300

    writeToAllBytes from 05 inspecting loops done

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,8 +1,28 @@
+#include "core_alloc.h"
+#include "core_exec_ctx.h"
 #include <core_init.h>
 #include <os_metrics.h>
 
 PRAGMA_WARNING_SUPPRESS_ALL
 
+struct Buffer {
+    addr_size count;
+    u8* data;
+};
+
+void writeToAllBytes(Buffer dest) {
+    for (u64 i = 0; i < dest.count; i++) {
+        dest.data[i] = u8(i);
+    }
+}
+
 i32 main() {
     coreInit();
+
+    Buffer buff;
+    buff.count = 100000;
+    buff.data = reinterpret_cast<u8*>(core::getAllocator(core::DEFAULT_ALLOCATOR_ID).alloc(buff.count, 1));
+    writeToAllBytes(buff);
+
+    return 0;
 }
